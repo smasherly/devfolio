bf715ba7444584c75db02f2ec2d74e42d4b9cdec
Ash Holbrook <ashholbrook@gmail.com>
2025-03-21 12:34:09 -0500

    update to purple

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,14 +5,14 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 const classes = {
   wrapper: 'block mb-6 md:flex',
-  imageWrapper: 'w-full max-w-150',
+  imageWrapper: 'w-24 md:w-full max-w-150',
   image: 'rounded-full transform transition-all duration-150 hover:scale-105',
   contentWrapper: 'flex-none pt-6 md:pt-1 md:flex-1 md:pl-20',
   name: 'text-5xl text-gray-900 font-bold leading-tight hover:text-black',
   description: 'text-gray-600',
   list: 'mt-6 uppercase tracking-wider',
   item: 'inline list-none pr-4',
-  link: 'inline-block py-2 font-semibold text-xs text-gray-600 hover:text-black',
+  link: 'inline-block py-2 font-semibold text-xs text-indigo-500 hover:text-black',
 };
 
 const Header = ({ metadata = {}, noBlog = false }) => {
