af707d7a8b4e59566a9aea59353804961821d9e6
Ash Holbrook <ashholbrook@gmail.com>
2025-03-26 12:37:22 -0500

    covert config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
@@ -9,3 +11,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default config;
